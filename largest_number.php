ac9151c098b49c86258e3b3ec0ea7e1e7ead3624
Tahsin Abdullah <tahsinabdullah107@gmail.com>
2024-07-08 21:34:05 +0600

    Add files via upload

---
+++ largest_number.php
@@ -0,0 +1,22 @@
+<?php
+$num1=30;
+$num2=70;
+$num3=50;
+if($num1>=$num2 && $num1>=$num3){
+    echo $num1;
+}elseif($num2>= $num1 && $num2>= $num3){
+    echo $num2;
+}else{
+   echo $num3;
+}
+// $num1 = 30;
+// $num2 = 20;
+// $num3 = 10;
+
+// if ($num1 >= $num2 && $num1 >= $num3) {
+//     echo $num1;
+// } elseif ($num2 >= $num1 && $num2 >= $num3) {
+//     echo $num2;
+// } else {
+//     echo $num3;
+// }
